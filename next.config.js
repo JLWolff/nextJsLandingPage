22284a24285b18d68894ff1836ba02e86ece44d0
JLWolff <joaolwolff@gmail.com>
2021-09-23 18:10:09 +0100

    changing base path

--- next.config.js
+++ next.config.js
@@ -2,3 +2,7 @@ const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
 module.exports = withPlugins([optimizedImages], { target: 'serverless' });
+module.exports = {
+    basePath: '/nextJsLandingPage',
+    assetPrefix: '/nextJsLandingPage'
+}
