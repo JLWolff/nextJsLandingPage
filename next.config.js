b9f3d0e3131e85c091c3f74a1165b578f09da51f
JLWolff <joaolwolff@gmail.com>
2021-09-23 18:33:47 +0100

    no jekyll config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
 module.exports = withPlugins([optimizedImages], { target: 'serverless' });
-module.exports = {
-    basePath: '/nextJsLandingPage',
-    assetPrefix: '/nextJsLandingPage'
-}
+// module.exports = {
+//     basePath: '/nextJsLandingPage',
+//     assetPrefix: '/nextJsLandingPage'
+// }
