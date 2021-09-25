816037b517a35d55c74b80da3743830e40c699ab
JLWolff <joaolwolff@gmail.com>
2021-09-25 12:08:26 +0100

    try to change static folder config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,33 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
+const withImages = require('next-images');
+const isDev = process.env.NODE_ENV === 'ISDEV'
 
+/*
+//  * Gets the BASE_PATH from the command used to start this app.
+//  * If BASE_PATH is specified but it does not start with a "/" 
+//  * then add it. 
+//  */
+// function getBasePath() {
+//     var basePath = ''
 
-module.exports = {
-    basePath: '/nextJsLandingPage',
-    assetPrefix: '/nextJsLandingPage'
-}
+//     if (!isDev && process.env.BASE_PATH){
+//         if (process.env.BASE_PATH.startsWith("/") ){
+//             basePath = process.env.BASE_PATH;
+//         } else {
+//             basePath = "/" + process.env.BASE_PATH;
+//         }
+//     } 
+
+//     console.log("getBasePath() : isProd = " + isProd);
+//     console.log("getBasePath() : basePath = " + basePath);
+
+//     return basePath
+// }
+
+module.exports = withImages({
+    staticFolder: '/nextJsLandingPage'
+
+});
+
 module.exports = withPlugins([optimizedImages], { target: 'serverless' });
